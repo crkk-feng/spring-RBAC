bb700bd793f582675876deace49ccc55c6c25a2a
crkk-feng <1460663026@qq.com>
2023-07-06 10:49:30 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+<div align="center">
+  <h1 align="center">
+    spring-RBAC
+  </h1>
+</div>
+
+说明：练习使用项目
+
+项目初始化
+
+
+
---
+++ src/test/java/com/relaper/cr/springrbac/SpringRbacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relaper.cr.springrbac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRbacApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
